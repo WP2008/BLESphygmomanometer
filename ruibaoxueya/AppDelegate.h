5805c6caff77ad6817167c8d1f889a846d1bbcd9
zbf <zbf@zbfdeMacBook-Air.local>
2014-04-15 21:29:10 +0800

    Initial Commit

---
+++ ruibaoxueya/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ruibaoxueya
+//
+//  Created by zbf on 14-4-15.
+//  Copyright (c) 2014年 zbf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
